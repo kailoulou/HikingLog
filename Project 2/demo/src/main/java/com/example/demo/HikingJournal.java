487c0ef3dee1732726d3a2e234856cf259fd89cf
Kailoulou <kdavis@colgate.edu>
2023-05-04 13:47:31 -0400

    Updated Maven import Calls

--- Project 2/demo/src/main/java/com/example/demo/HikingJournal.java
+++ Project 2/demo/src/main/java/com/example/demo/HikingJournal.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.sothawo.mapjfx.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
--- Project 2/demo/src/main/java/module-info.java
+++ Project 2/demo/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.demo {
         requires org.controlsfx.controls;
     requires java.logging;
     requires com.opencsv;
+    requires com.sothawo.mapjfx;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
